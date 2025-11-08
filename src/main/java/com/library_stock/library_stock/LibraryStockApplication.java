df9d4394c7efa1b1161da628169ba8dfd4cd07b0
tiago <tiago.taumaturgo@gmail.com>
2025-11-08 09:59:05 -0300

    first commit

---
+++ src/main/java/com/library_stock/library_stock/LibraryStockApplication.java
@@ -0,0 +1,13 @@
+package com.library_stock.library_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library_stock/library_stock/LibraryStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library_stock.library_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
