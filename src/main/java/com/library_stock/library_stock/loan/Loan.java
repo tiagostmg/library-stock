0cee00672c49fdf7d01f3c927380b435ac2df229
Tiago <tiago.taumaturgo@gmail.com>
2025-11-16 14:09:44 -0300

    fix: LoanService

--- src/main/java/com/library_stock/library_stock/loan/Loan.java
+++ src/main/java/com/library_stock/library_stock/loan/Loan.java
@@ -38,17 +38,16 @@ public class Loan {
     @Column()
     private String notes;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "reader_id", nullable = false)
     private Reader reader;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_instance_id", nullable = false)
     private BookInstance bookInstance;
-
 
 }
--- src/main/java/com/library_stock/library_stock/loan/LoanService.java
+++ src/main/java/com/library_stock/library_stock/loan/LoanService.java
@@ -1,6 +1,14 @@
 package com.library_stock.library_stock.loan;
 
 import com.library_stock.library_stock.base.BaseService;
+import com.library_stock.library_stock.book.Book;
+import com.library_stock.library_stock.book.viewModel.BookViewModel;
+import com.library_stock.library_stock.bookInstance.BookInstance;
+import com.library_stock.library_stock.bookInstance.viewModel.BookInstanceViewModel;
+import com.library_stock.library_stock.loan.viewModel.ReturnOverduenViewModel;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 public class LoanService extends BaseService<Loan, Integer, LoanRepository>{
@@ -9,6 +17,61 @@ public class LoanService extends BaseService<Loan, Integer, LoanRepository>{
         super(repository);
     }
 
+    public List<ReturnOverduenViewModel> overduenLoan() {
 
+        List<Loan> overdueLoans = repository
+                .findByActualReturnDateIsNullAndExpectedReturnDateBefore(LocalDate.now());
+
+        return overdueLoans.stream()
+                .map(this::mapToReturnOverdueViewModel)
+                .toList();
+    }
+
+    private ReturnOverduenViewModel mapToReturnOverdueViewModel(Loan loan) {
+        ReturnOverduenViewModel vm = new ReturnOverduenViewModel();
+
+        vm.setLoanDate(loan.getLoanDate());
+        vm.setExpectedReturnDate(loan.getExpectedReturnDate());
+
+        // Converter Book -> BookViewModel
+        vm.setBook(mapToBookViewModel(loan.getBookInstance().getBook()));
+
+        // Converter BookInstance -> BookInstanceViewModel
+        vm.setBookInstance(mapToBookInstanceViewModel(loan.getBookInstance()));
+
+        return vm;
+    }
+
+    private BookViewModel mapToBookViewModel(Book book) {
+        BookViewModel vm = new BookViewModel();
+
+        vm.setId(book.getId());
+        vm.setTitle(book.getTitle());
+        vm.setAuthor(book.getAuthor());
+        vm.setPublisher(book.getPublisher());
+        vm.setIsbn(book.getIsbn());
+        vm.setCategory(book.getCategory());
+        vm.setNotes(book.getNotes());
+
+        return vm;
+    }
+
+    private BookInstanceViewModel mapToBookInstanceViewModel(BookInstance instance) {
+        BookInstanceViewModel vm = new BookInstanceViewModel();
+
+        vm.setId(instance.getId());
+        vm.setInternalCode(instance.getInternalCode());
+        vm.setAcquisitionDate(instance.getAcquisitionDate());
+        vm.setPreservationState(instance.getPreservationState());
+        vm.setStatus(instance.getStatus());
+
+        // book dentro do instance
+        vm.setBook(mapToBookViewModel(instance.getBook()));
+
+        // location dentro do instance
+        vm.setLocation(instance.getLocation());
+
+        return vm;
+    }
 
 }
