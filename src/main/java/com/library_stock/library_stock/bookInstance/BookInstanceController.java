5233bd1f5a7fca04bbe071af40a82eab42fa8410
Tiago <tiago.taumaturgo@gmail.com>
2025-11-26 09:18:53 -0300

    fix: book-instances endpoint

--- src/main/java/com/library_stock/library_stock/bookInstance/BookInstanceController.java
+++ src/main/java/com/library_stock/library_stock/bookInstance/BookInstanceController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bookInstances")
+@RequestMapping("/book-instances")
 public class BookInstanceController {
 
     @Autowired
