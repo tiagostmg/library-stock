b1bae5435ee25e8b6104cf68c96cc4c426dd9b63
Tiago <tiago.taumaturgo@gmail.com>
2025-11-27 13:19:08 -0300

    refactor: OverdueLoans

--- src/main/java/com/library_stock/library_stock/loan/LoanService.java
+++ src/main/java/com/library_stock/library_stock/loan/LoanService.java
@@ -1,15 +1,12 @@
 package com.library_stock.library_stock.loan;
 
 import com.library_stock.library_stock.base.BaseService;
-import com.library_stock.library_stock.book.Book;
 import com.library_stock.library_stock.bookInstance.BookInstance;
 import com.library_stock.library_stock.loan.types.LoanStatus;
 import com.library_stock.library_stock.bookInstance.BookInstanceRepository;
 import com.library_stock.library_stock.bookInstance.types.BookStatus;
-import com.library_stock.library_stock.bookInstance.viewModel.BookInstanceViewModel;
-import com.library_stock.library_stock.book.viewModel.BookViewModel;
 import com.library_stock.library_stock.loan.viewModel.BorrowBookResponseViewModel;
-import com.library_stock.library_stock.loan.viewModel.ReturnOverdueViewModel;
+import com.library_stock.library_stock.loan.viewModel.OverdueResponseViewModel;
 import org.springframework.stereotype.Service;
 import com.library_stock.library_stock.reader.Reader;
 import com.library_stock.library_stock.reader.ReaderRepository;
@@ -89,61 +86,15 @@ public class LoanService extends BaseService<Loan, Integer, LoanRepository>{
     }
 
 
-    public List<ReturnOverdueViewModel> overduenLoan() {
+    public List<OverdueResponseViewModel> getOverdueLoans() {
 
         List<Loan> overdueLoans = repository
                 .findOverdueLoans(LocalDate.now());
 
         return overdueLoans.stream()
-                .map(this::mapToReturnOverdueViewModel)
+                .map(e -> new OverdueResponseViewModel().toViewModel(e))
                 .toList();
     }
 
-    private ReturnOverdueViewModel mapToReturnOverdueViewModel(Loan loan) {
-        ReturnOverdueViewModel vm = new ReturnOverdueViewModel();
-
-        vm.setLoanDate(loan.getLoanDate());
-        vm.setExpectedReturnDate(loan.getExpectedReturnDate());
-
-        // Converter Book -> BookViewModel
-        vm.setBook(mapToBookViewModel(loan.getBookInstance().getBook()));
-
-        // Converter BookInstance -> BookInstanceViewModel
-        vm.setBookInstance(mapToBookInstanceViewModel(loan.getBookInstance()));
-
-        return vm;
-    }
-
-    private BookViewModel mapToBookViewModel(Book book) {
-        BookViewModel vm = new BookViewModel();
-
-        vm.setId(book.getId());
-        vm.setTitle(book.getTitle());
-        vm.setAuthor(book.getAuthor());
-        vm.setPublisher(book.getPublisher());
-        vm.setIsbn(book.getIsbn());
-        vm.setCategory(book.getCategory());
-        vm.setNotes(book.getNotes());
-
-        return vm;
-    }
-
-    private BookInstanceViewModel mapToBookInstanceViewModel(BookInstance instance) {
-        BookInstanceViewModel vm = new BookInstanceViewModel();
-
-        vm.setId(instance.getId());
-        vm.setInternalCode(instance.getInternalCode());
-        vm.setAcquisitionDate(instance.getAcquisitionDate());
-        vm.setPreservationState(instance.getPreservationState());
-        vm.setStatus(instance.getStatus());
-
-        // book dentro do instance
-        vm.setBook(mapToBookViewModel(instance.getBook()));
-
-        // location dentro do instance
-        vm.setLocation(instance.getLocation());
-
-        return vm;
-    }
 
 }
