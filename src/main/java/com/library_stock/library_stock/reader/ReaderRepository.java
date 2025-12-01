6fa07e23e6352fd2f668e5bc61cbda32da2f061f
Tiago <tiago.taumaturgo@gmail.com>
2025-12-01 11:00:56 -0300

    feat: fix borrow book and reader logics

--- src/main/java/com/library_stock/library_stock/reader/ReaderRepository.java
+++ src/main/java/com/library_stock/library_stock/reader/ReaderRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReaderRepository extends JpaRepository<Reader, Integer> {
+    Optional<Reader> findByCpf(String cpf);
+
     Page<Reader> findByCpfContainingIgnoreCase(String cpf, Pageable pageable);
 
     Page<Reader> findByNameContainingIgnoreCase(String name, Pageable pageable);
